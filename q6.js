159851890bfe1da2e550204d0e45967ff2c06883
yagelcohen <77248177+yagelcohen@users.noreply.github.com>
2021-01-19 19:52:43 +0200

    answer q6

--- q6.js
+++ q6.js
@@ -1,11 +1,20 @@
+"use strict"
 function run() {
     const input = document.getElementById("input").value;
     const output = document.getElementById("output");
     let out = "";
-/**/
-    // write your code here
-    // out = input + 5;
 
-/**/
+    const numbersArr=JSON.parse(input);
+    //15814
+    let answer = [numbersArr[0]];
+    for(let i=1; i<numbersArr.length;i++){
+        if((numbersArr[i-1]%2 === 0)&&(numbersArr[i]%2 === 0)){
+            answer.push('-', numbersArr[i]);
+        }else{
+            answer.push(numbersArr[i]);
+        }
+    
+    }
+         out= answer;
     output.innerText = out;
 }
