e0524a62f4c5c5f88dd96beeded56be9b22d98c5
yagelcohen <77248177+yagelcohen@users.noreply.github.com>
2021-01-19 23:58:07 +0200

    not finish q13

--- q13.js
+++ q13.js
@@ -1,11 +1,11 @@
+"use strict"
+const array = []
+array.push(input)
+
 function run() {
     const input = document.getElementById("input").value;
     const output = document.getElementById("output");
     let out = "";
-/**/
-    // write your code here
-    // out = input + 5;
 
-/**/
     output.innerText = out;
 }
