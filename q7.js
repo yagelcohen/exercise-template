71d1d0591129e52f94cbab1221e9535cc2262b04
yagelcohen <77248177+yagelcohen@users.noreply.github.com>
2021-01-19 20:32:24 +0200

    answer q7

--- q7.js
+++ q7.js
@@ -2,10 +2,12 @@ function run() {
     const input = document.getElementById("input").value;
     const output = document.getElementById("output");
     let out = "";
-/**/
-    // write your code here
-    // out = input + 5;
-
-/**/
+    const numbersArr=JSON.parse(input);
+    numbersArr.sort(sorts);
+    out=numbersArr;
     output.innerText = out;
 }
+function sorts(a, b){
+    return a - b;
+}
+
