06eb709259702dbf80e0f04cc33cbd9815d7779c
yagelcohen <77248177+yagelcohen@users.noreply.github.com>
2021-01-19 17:43:27 +0200

    answer q3

--- q3.js
+++ q3.js
@@ -1,11 +1,18 @@
+"use strict"
 function run() {
     const input = document.getElementById("input").value;
+    const n = document.getElementById("n").value;
     const output = document.getElementById("output");
-    let out = "";
-/**/
-    // write your code here
-    // out = input + 5;
+    let out = " ";
+    const array=JSON.parse(input);
+        console.log(JSON.parse(input));
+      if(n === null){
+         out=array[0];
+      }else if(n>0){
+         out=array.slice(0, n);
+          }else{
+              out=[];
+          }
 
-/**/
     output.innerText = out;
 }
