8b16792d776bd046a25c694d42bf3f234c5ef74f
yagelcohen <77248177+yagelcohen@users.noreply.github.com>
2021-01-20 13:50:39 +0200

    answer q18

--- q18.js
+++ q18.js
@@ -1,11 +1,14 @@
-function run() {
-    const input = document.getElementById("input").value;
-    const output = document.getElementById("output");
-    let out = "";
-/**/
-    // write your code here
-    // out = input + 5;
+// function run() {
+//     const input = document.getElementById("input").value;
+//     const output = document.getElementById("output");
+//     let out = "";
+//     output.innerText = out;
+// }
 
-/**/
-    output.innerText = out;
+const myArray= [1, 2, 3, 4, 5, 7, 8, 9];
+    let x = 4
+    for(i=0; i<myArray.length; i++){
+        if(x === myArray[i]){
+        console.log(i);
+  }
 }
