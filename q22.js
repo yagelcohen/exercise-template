9a5284a3f07fe68d97899d9ab05528d90a3f3259
yagelcohen <77248177+yagelcohen@users.noreply.github.com>
2021-01-20 16:37:29 +0200

    answer q22

--- q22.js
+++ q22.js
@@ -1,11 +1,23 @@
-function run() {
-    const input = document.getElementById("input").value;
-    const output = document.getElementById("output");
-    let out = "";
-/**/
-    // write your code here
-    // out = input + 5;
+// function run() {
+//     const input = document.getElementById("input").value;
+//     const output = document.getElementById("output");
+//     let out = ""; 
+//     output.innerText = out;
+// }
 
-/**/
-    output.innerText = out;
-}
+
+
+    const myArray= [100, 2, 1, 10]
+    const isArray=[10, 2, 1 , 3]
+    let newArray=[]
+
+   function pushArr(newArray, myArray){
+       for(i=0; i< myArray.length; i++){
+           if(!(newArray.includes(myArray[i]))){
+            newArray.push(myArray[i])
+           }
+       }
+   }
+   pushArr(newArray,myArray);
+   pushArr(newArray,isArray);
+//    console.log(newArray)
